16b7f7f294d8fd530386e6123502a0ccc7214a73
Drew Heavner <veedubusc@gmail.com>
2024-06-15 23:38:02 -0400

    Bootstrappin

--- systems/aarch64-linux/nixpi/default.nix
+++ systems/aarch64-linux/nixpi/default.nix
@@ -1,7 +1,74 @@
 { libs, pkgs, config, ... }:
 let
-  user = "r0adkll";
   hostname = "nixpi";
 in {
   
+  imports = [
+    "${fetchTarball "https://github.com/NixOS/nixos-hardware/tarball/master"}/raspberry-pi/4"
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    kernelParams = [
+      # This device is stationed on a vertical monitor, so we rotate the terminal window to 270deg
+      "fbcon=rotate:3" 
+    ];
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = hostname;
+    wireless.enable = true;
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  programs = {
+    fish.enable = true;
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+
+    eternal-terminal.enable = true;
+  };
+
+  users = [
+    r0adkll = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      shell = pkgs.fish;
+      initialPassword = "pass";
+      openssh.authorizedKeys.keys = [ (builtins.readFile keys/dhMBP.pub) ];
+    };
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  system = {
+    # NEVER change this value after the initial install, for any reason,
+    stateVersion = "23.11"; # Did you read the comment?
+  };
 }
