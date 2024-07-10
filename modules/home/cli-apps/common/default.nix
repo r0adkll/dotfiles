1a44690c0ef67ed998b1658e774a6bd9a07b0cfc
r0adkll <veedubusc@gmail.com>
2024-07-10 16:42:44 -0400

    Moved starship config to toml file

--- modules/home/cli-apps/common/default.nix
+++ modules/home/cli-apps/common/default.nix
@@ -70,6 +70,8 @@ in {
 
     fonts.fontconfig.enable = true;
 
+    xdg.configFile."starship.toml".source = ./configs/starship.toml;
+
     programs = {
       fish = {
         enable = true;
@@ -86,63 +88,6 @@ in {
         enable = true;
         enableFishIntegration = true;
         enableTransience = true;
-
-        settings = {
-          format = "$all$directory$character";
-
-          username = {
-            format = "[$user]($style) ";
-          };
-
-          hostname = {
-            style = "bold bright-red";
-            ssh_symbol = " ";
-          };
-
-          aws.symbol = "  ";
-          buf.symbol = " ";
-          c.symbol = " ";
-          conda.symbol = " ";
-          crystal.symbol = " ";
-          dart.symbol = " ";
-          directory.read_only = " 󰌾";
-          docker_context.symbol = " ";
-          elixir.symbol = " ";
-          elm.symbol = " ";
-          fennel.symbol = " ";
-          fossil_branch.symbol = " ";
-          git_branch.symbol = " ";
-          golang.symbol = " ";
-          guix_shell.symbol = " ";
-          haskell.symbol = " ";
-          haxe.symbol = " ";
-          hg_branch.symbol = " ";
-          java.symbol = " ";
-          julia.symbol = " ";
-          kotlin.symbol = " ";
-          lua.symbol = " ";
-          memory_usage.symbol = "󰍛 ";
-          meson.symbol = "󰔷 ";
-          nim.symbol = "󰆥 ";
-          nix_shell.symbol = " ";
-          nodejs.symbol = " ";
-          ocaml.symbol = " ";
-          os.symbols = {
-            Android = " ";
-            NixOS = " ";
-          };
-          package.symbol = "󰏗 ";
-          perl.symbol = " ";
-          php.symbol = " ";
-          pijul_channel.symbol = " ";
-          python.symbol = " ";
-          rlang.symbol = "󰟔 ";
-          ruby.symbol = " ";
-          rust.symbol = " ";
-          scala.symbol = " ";
-          swift.symbol = " ";
-          zig.symbol = " ";
-        };
       };
 
       helix = {
